a51e5b539ee7c9ae8911a64dee786f400ff08b0f
Pedro Bufulin <pedro.almeida@ufu.br>
2020-02-11 21:56:18 -0300

    corrigido bug na verificação de 5 entregas

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -1,13 +1,17 @@
-import { format } from 'date-fns';
+import { format, parseISO } from 'date-fns';
 
 import PackageLimit from '../schemas/PackageLimit';
 import Package from '../models/Package';
 
 class DeliveryController {
     async addStart(req, res) {
-        const { package_id, courier_id, reqDate = new Date() } = req.body;
-
+        const { package_id, courier_id } = req.body;
         const delivery = await Package.findByPk(package_id);
+
+        let { reqDate } = req.body;
+        // essa linha abaixo é só para propósitos de teste,
+        // remover quando for para a aplicação
+        reqDate = reqDate ? parseISO(reqDate) : new Date();
 
         if (delivery.courier_id !== courier_id) {
             return res.json({
@@ -29,24 +33,23 @@ class DeliveryController {
             });
         }
 
-        if (limiter.packagesTakenIds.length === 5) {
-            return res.json({
-                error: 'this courier is trying to exceed the limit',
-            });
-        }
-
         const date1 = format(reqDate, 'dd/MM/yyyy');
         const date2 = format(limiter.takenDate, 'dd/MM/yyyy');
 
         if (date1 === date2) {
-            let array = limiter.packagesTakenIds;
+            if (limiter.packagesTakenIds.length === 5) {
+                return res.json({
+                    error: 'this courier is trying to exceed the limit',
+                });
+            }
+            const array = limiter.packagesTakenIds;
             array.push(package_id);
             await limiter.update({
                 packagesTakenIds: array,
             });
         } else {
             await limiter.update({
-                packagesTakenIds: [],
+                packagesTakenIds: [package_id],
                 takenDate: reqDate,
             });
         }
