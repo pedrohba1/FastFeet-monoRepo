3f743a6e3fabf52b629109dc0640097ae8b1657d
pedro Bufulin <pedro.almeida@ufu.br>
2020-03-22 01:04:23 -0300

    configurado o redux e o redux-saga

--- frontend/fasfeet-web/src/App.js
+++ frontend/fasfeet-web/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './config/ReactotronConfig';
 
 import history from './services/history';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+import store from './store';
 
 function App() {
     return (
-        <Router history={history}>
-            <Routes />
-            <GlobalStyle />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes />
+                <GlobalStyle />
+            </Router>
+        </Provider>
     );
 }
 
