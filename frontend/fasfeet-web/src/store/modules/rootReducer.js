7fbfa9d87810d282d04d39e14c25218ade48f9f6
pedro Bufulin <pedro.almeida@ufu.br>
2020-03-22 21:44:50 -0300

    persistor e reducer de user

--- frontend/fasfeet-web/src/store/modules/rootReducer.js
+++ frontend/fasfeet-web/src/store/modules/rootReducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
-import auth from './auth/reducer';
 
-export default combineReducers({ auth });
+import auth from './auth/reducer';
+import user from './user/reducer';
+
+export default combineReducers({ auth, user });
