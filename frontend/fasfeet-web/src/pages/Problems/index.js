4836df708bed192e734df9d06b72776e689c6d3c
pedro Bufulin <pedro.almeida@ufu.br>
2020-03-25 03:20:37 -0300

    esqueleto das págnas preparado

--- frontend/fasfeet-web/src/pages/Problems/index.js
+++ frontend/fasfeet-web/src/pages/Problems/index.js
@@ -1,11 +1,62 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useState, useEffect } from 'react';
+import {
+    Title,
+    Buttons,
+    RegisterButton,
+    SearchIcon,
+    Search,
+} from '~/styles/default';
+import api from '~/services/api';
 
 export default function Problems() {
+    const [problems, setProblems] = useState([]);
+    const [input, setInput] = useState('');
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+
+    async function searchPackages() {
+        setLoading(true);
+
+        /*     const response = await api.get('recipients', {
+            params: {
+                page,
+                product: input,
+                courierName: input,
+                recipientName: input,
+            },
+        });
+
+        response.data.map(
+            // eslint-disable-next-line no-return-assign
+            pack => pack.id < 10 && (pack.id = `0${pack.id}`)
+        );
+        setProblems(response.data); */
+        setLoading(false);
+    }
+    useEffect(() => {
+        searchPackages();
+    }, [page]);
+
+    function handleEnterPress(e) {
+        if (e.which === 13 || e.keyCode === 13) {
+            searchPackages();
+        }
+    }
+
     return (
-        <div>
-            <h1>Problems</h1>
-        </div>
+        <>
+            <Title>Problemas na entrega</Title>
+
+            <Buttons>
+                <Search
+                    loading={loading}
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                    onKeyUp={handleEnterPress}
+                    placeholder="buscar por destinatários"
+                    iconPosition="left"
+                />
+            </Buttons>
+        </>
     );
 }
--- frontend/fasfeet-web/src/pages/Recipients/index.js
+++ frontend/fasfeet-web/src/pages/Recipients/index.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+    Title,
+    Buttons,
+    RegisterButton,
+    SearchIcon,
+    Search,
+} from '~/styles/default';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+export default function Recipients() {
+    const [recipients, setRecipients] = useState([]);
+    const [input, setInput] = useState('');
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
-export default function Recipíents() {
+    async function searchPackages() {
+        setLoading(true);
+
+        /*      const response = await api.get('recipients', {
+            params: {
+                page,
+                product: input,
+                courierName: input,
+                recipientName: input,
+            },
+        });
+
+        response.data.map(
+            // eslint-disable-next-line no-return-assign
+            pack => pack.id < 10 && (pack.id = `0${pack.id}`)
+        );
+        setRecipients(response.data); */
+        setLoading(false);
+    }
+    useEffect(() => {
+        searchPackages();
+    }, [page]);
+
+    function handleEnterPress(e) {
+        if (e.which === 13 || e.keyCode === 13) {
+            searchPackages();
+        }
+    }
+
     return (
-        <div>
-            <h1>Recipients</h1>
-        </div>
+        <>
+            <Title>Gerenciando destinatários</Title>
+
+            <Buttons>
+                <Search
+                    loading={loading}
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                    onKeyUp={handleEnterPress}
+                    placeholder="buscar por destinatários"
+                    iconPosition="left"
+                />
+                <RegisterButton>
+                    <SearchIcon />
+                    <span>CADASTRAR</span>
+                </RegisterButton>
+            </Buttons>
+        </>
     );
 }
