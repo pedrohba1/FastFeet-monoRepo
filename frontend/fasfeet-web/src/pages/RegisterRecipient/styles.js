ab1859e082f07e9c27fa4b5a150783e32dd3c3e7
pedro Bufulin <pedro.almeida@ufu.br>
2020-03-30 17:00:54 -0300

    formulário de destinatários funcional

--- frontend/fasfeet-web/src/pages/RegisterRecipient/styles.js
+++ frontend/fasfeet-web/src/pages/RegisterRecipient/styles.js
@@ -1,20 +1,24 @@
 import styled from 'styled-components';
 import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-self: center;
+    margin: auto;
+    max-width: 1200px;
+`;
 
 export const Header = styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
+    justify-content: space-between;
 `;
 
 export const Buttons = styled.div`
     display: flex;
     flex-direction: row;
 `;
-
-export const DataContainer = styled.div``;
 
 export const BackButton = styled.button`
     display: flex;
@@ -36,4 +40,34 @@ export const BackButton = styled.button`
     padding: 0 10px;
     font-weight: bold;
     border-radius: 4px;
+`;
+
+export const FormContainer = styled.div`
+    background: #ffffff;
+    border-radius: 4px;
+    padding: 30px;
+`;
+
+export const InputContainer = styled.div`
+    width: ${props => props.width};
+    padding: 10px;
+    input {
+        padding: 0 15px;
+        height: 45px;
+        border-radius: 4px;
+        border: 1px solid #dddddd;
+        display: flex;
+        width: 100%;
+    }
+`;
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+`;
+
+export const InputLabel = styled.h4`
+    color: #444;
+    font-weight: bold;
 `;
