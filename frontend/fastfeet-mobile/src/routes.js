212772ea43a379a8a6887f864eeb5c363d3b6f14
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-02 15:04:01 -0300

    adicionei o projeto mobile, com o script de ouvir o emulador na porta do backend

---
+++ frontend/fastfeet-mobile/src/routes.js
@@ -0,0 +1,30 @@
+// In App.js in a new project
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import colors from './styles/colors';
+
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import ShoesHeader from './components/ShoesHeader';
+
+const Stack = createStackNavigator();
+
+function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    header: () => <ShoesHeader />,
+                }}
+            >
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Cart" component={Cart} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default Routes;
