00c58802e7c814e7633b2ef79705dedf195cb94d
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-14 17:26:27 -0300

    tela de detalhes de encomenda

--- frontend/fastfeet-mobile/src/pages/Deliveries/PackDetails/index.js
+++ frontend/fastfeet-mobile/src/pages/Deliveries/PackDetails/index.js
@@ -1,12 +1,54 @@
-import React, { useLayoutEffect } from 'react';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { useLayoutEffect, useMemo } from 'react';
+import HIcon from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { TouchableOpacity } from 'react-native';
-import { Container } from './styles';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import {
+    Container,
+    HorizontalContainer,
+    HText,
+    LabelText,
+    Background,
+    ContentText,
+    TextContainer,
+    HTextContainer,
+    Buttons,
+    Button,
+} from './styles';
 
 export default function PackDetails({ navigation, route }) {
     const { data } = route.params;
 
     console.tron.log(data);
+
+    const startDateFormatted = useMemo(() => {
+        return data.start_date !== null
+            ? format(parseISO(data.start_date), 'dd/MM/yyyy', {
+                  locale: pt,
+              })
+            : '--/--/--';
+    }, [data.start_date]);
+
+    const endDateFormatted = useMemo(() => {
+        return data.end_date !== null
+            ? format(parseISO(data.end_date), 'dd/MM/yyyy', {
+                  locale: pt,
+              })
+            : '--/--/--';
+    }, [data.end_date]);
+
+    const status = useMemo(() => {
+        if (data.start_date === null) {
+            return 'não retirado';
+        }
+        if (data.start_date !== null) {
+            return 'pendente';
+        }
+        if (data.end_date !== null) {
+            return 'entregue';
+        }
+    }, [data.start_udate, data.start_date, data.end_date]);
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -16,11 +58,93 @@ export default function PackDetails({ navigation, route }) {
                         navigation.goBack();
                     }}
                 >
-                    <Icon name="chevron-left" size={30} color="#fff" />
+                    <HIcon name="chevron-left" size={30} color="#fff" />
                 </TouchableOpacity>
             ),
         });
     }, []);
 
-    return <Container />;
+    return (
+        <Background>
+            <Container>
+                <HorizontalContainer>
+                    <Icon name="truck" size={20} color="#7D40E7" />
+                    <HText>Informações de entrega</HText>
+                </HorizontalContainer>
+
+                <TextContainer>
+                    <LabelText>DESTINATÁRIO</LabelText>
+                    <ContentText>{data.recipient.name}</ContentText>
+                </TextContainer>
+
+                <TextContainer>
+                    <LabelText>ENDEREÇO DE ENTREGA</LabelText>
+                    <ContentText>
+                        {data.recipient.address},{' '}
+                        {data.recipient.address_number},{' '}
+                        {data.recipient.address_complement},{' '}
+                        {data.recipient.city} - {data.recipient.state},{' '}
+                        {data.recipient.cep}
+                    </ContentText>
+                </TextContainer>
+
+                <TextContainer>
+                    <LabelText>PRODUTO</LabelText>
+                    <ContentText>{data.product}</ContentText>
+                </TextContainer>
+            </Container>
+
+            <Container>
+                <HorizontalContainer>
+                    <Icon name="calendar-outline" size={20} color="#7D40E7" />
+                    <HText>Situação de entrega</HText>
+                </HorizontalContainer>
+
+                <TextContainer>
+                    <LabelText>STATUS</LabelText>
+                    <ContentText>{status}</ContentText>
+                </TextContainer>
+
+                <HTextContainer>
+                    <TextContainer>
+                        <LabelText>DATA DE RETIRADA</LabelText>
+                        <ContentText>{startDateFormatted}</ContentText>
+                    </TextContainer>
+                    <TextContainer>
+                        <LabelText>DATA DE ENTREGA</LabelText>
+                        <ContentText>{endDateFormatted}</ContentText>
+                    </TextContainer>
+                </HTextContainer>
+            </Container>
+
+            <Buttons>
+                <Container>
+                    <Button>
+                        <Icon
+                            name="close-circle-outline"
+                            size={40}
+                            color="#E74040"
+                        />
+                        <ContentText>Informar{'\n'}Problema</ContentText>
+                    </Button>
+                </Container>
+                <Container>
+                    <Button>
+                        <Icon
+                            name="information-outline"
+                            size={40}
+                            color="#E7BA40"
+                        />
+                        <ContentText>Informar{'\n'}Problema</ContentText>
+                    </Button>
+                </Container>
+                <Container>
+                    <Button>
+                        <Icon name="check-circle" size={40} color="#7D40E7" />
+                        <ContentText>Informar{'\n'}Problema</ContentText>
+                    </Button>
+                </Container>
+            </Buttons>
+        </Background>
+    );
 }
--- frontend/fastfeet-mobile/src/pages/Deliveries/PackDetails/styles.js
+++ frontend/fastfeet-mobile/src/pages/Deliveries/PackDetails/styles.js
@@ -1,3 +1,70 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Background = styled.SafeAreaView`
+    background: #ffffff;
+    flex: 1;
+`;
+
+export const TextContainer = styled.View`
+    margin: 5px 0;
+`;
+
+export const HTextContainer = styled.View`
+    margin: 5px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const Container = styled.View`
+    padding: 8px;
+    display: flex;
+    align-self: stretch;
+    border-width: 0.5px;
+    border-radius: 0.5px;
+    border-color: #ddd;
+    border-bottom-width: 0;
+    shadow-color: #000;
+    shadow-opacity: 0.8;
+    shadow-radius: 1px;
+    elevation: 0.2;
+    margin-left: 5px;
+    margin-right: 5px;
+    margin-top: 10px;
+`;
+
+export const Buttons = styled.View`
+    align-self: center;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+`;
+
+export const HorizontalContainer = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-self: stretch;
+`;
+
+export const HText = styled.Text`
+    margin-left: 5px;
+    font-weight: bold;
+    color: #7d40e7;
+`;
+
+export const LabelText = styled.Text`
+    color: #999999;
+    font-weight: bold;
+`;
+
+export const ContentText = styled.Text`
+    color: #666666;
+`;
+
+export const Button = styled.TouchableOpacity`
+    align-items: center;
+    display: flex;
+    align-self: stretch;
+    flex-direction: column;
+`;
